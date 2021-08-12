c3c31ae14a0c5d31a2b25b338f1dd9ae5552e3cf
dylson <dylsonuni@gmail.com>
2021-08-12 22:30:11 +0100

    dwdwwddw

--- index.js
+++ index.js
@@ -1,23 +1,6 @@
-//Write a function that takes in a number and returns the corresponging day of the week. 
-//Ex:
-//Input: 1
-//Output: 'Monday'
-//Input: 5
-//Output: 'Friday'
-//Input: 8
-//Output: undefined
+const apple = "apple";
+var fruit = "banana";
 
-function findDOWNumber (dowNUM) {
-    /* we give a number and return a string. We are creating a map */
+var fruitSalad = "";
 
-    var dowList = ["Sunday", "Monday", "Tuesday", "Wednesday"];
-
-    return dowList[dowNUM];
-
-
-
-};
-
-console.log("Expected Monday, Actual: " + findDOWNumber(1));
-console.log("Expected Friday, Actual: " + findDOWNumber(5));
-console.log("Expected undefined, Actual: " + findDOWNumber(8));
+fruitSalad = apple + fruit;
